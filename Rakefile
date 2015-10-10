50d9617e30e0f79d63f46e4c1f45a4b641b17b13
octacious <octacious@users.noreply.github.com>
2015-10-10 20:32:13 +0200

    Move I/O code to the Rakefile.

--- Rakefile
+++ Rakefile
@@ -1,9 +1,18 @@
 directory 'gen'
 
 task :default => 'gen' do
+	require './scripts/generate'
+
 	Dir.glob('posts/**/*.txt') do |post_path|
-		`ruby scripts/generate.rb #{post_path}`
-		basename = File.basename(post_path, '.txt')
-		puts "=> #{basename}"
+		# Remove the first part of the path, so that
+		# the posts/ hierarchy will essentially be 'copied' over
+		# to gen/.
+		dirname, basename = File.split(post_path)
+		dirname['posts'] = ''
+
+		html_path = File.join('gen/', dirname, basename.sub(/\.txt\Z/, '.html'))
+		html = Blogger.generate_html(IO.read(post_path))
+
+		IO.write(html_path, html)
 	end
 end
--- scripts/generate.rb
+++ scripts/generate.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
-class String
-  def decorate(left, right)
-    "#{left}#{self}#{right}"
+module Blogger
+
+  def self.tag(name, *contents)
+    "<#{name}>#{contents.join}</#{name}>"
   end
+
+  def self.generate_html(post_text)
+    tag('html',
+      tag('head', tag('title', 'A post')),
+      tag('body', tag('p', post_text)))
+  end
+
 end
-
-# Read the post and convert it to HTML.
-raise "Usage: #{$0} filename.txt" if ARGV.empty?
-text = File.read(ARGV[0])
-html = '<html><head><title>A post</title></head><body>' + text.decorate('<p>', '</p>') + '</body></html>'
-
-basename = File.basename(ARGV[0], '.txt')
-File.write(File.join('gen', basename + '.html'), html)
